547515ebd03c28bb966da52a9e8e8e8672293407
yous <youssefhero21@gmail.com>
2025-05-12 20:12:15 +0100

    fair enough

---
+++ ex07/r_dwssap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat /etc/passwd | \
+grep -v '^#' | \
+sed -n 'n;p' | \
+cut -d ':' -f1 | \
+rev | \
+sort -r | \
+sed -n "${FT_LINE1},${FT_LINE2}p" | \
+paste -sd ", " - | \
+sed 's/$/./'
+
