f31ed56bbcb56f54ef491cafd1f82cf35086d370
yous <youssefhero21@gmail.com>
2025-05-14 17:07:59 +0100

    exerice 07

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+FT_LINE1=2
+FT_LINE2=5
 
 cat /etc/passwd | \
-grep -v '^#' | \
-sed -n 'n;p' | \
-cut -d ':' -f1 | \
+sed '/^#/d' | \
+awk 'NR % 2 == 0' | \
+awk -F ':' '{print $1}' | \
 rev | \
-sort -r | \
-sed -n "${FT_LINE1},${FT_LINE2}p" | \
-paste -sd ", " - | \
-sed 's/$/./'
-
+sort -fnr | \
+awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2" 'NR >= FT_LINE1 && NR <= FT_LINE2' | \
+tr '\n' ' ' | \
+sed 's+ +, +g' | \
+sed 's+, $+.+g' | \
+tr -d '\n'
